023fbd634c01c238115ab5482629641a3b68aeee
Wth166017 <1660172550@qq.com>
2026-04-10 22:13:10 +0800

    学习了定时器

---
+++ 4-7定时器定时中断/User/main.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h"
+#include "Delay.h"
+#include "OLED.h"
+#include "Timer.h"
+uint16_t Num;
+int main(void)
+{
+	OLED_Init();
+    Timer_Init();
+	OLED_ShowString(1,1,"NUM:");
+	
+	while(1)
+    {
+		OLED_ShowNum(1,5,Num,5);
+	}
+}
